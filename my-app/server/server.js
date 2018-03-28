a056692352a0fe12ac47dbaf83051d7f07f10af3
[mike-gross-0809] <michaelgross8732@gmail.com>
2018-03-28 19:57:53 -0400

    Add server

---
+++ my-app/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cors());
+
+const port = 3000;
+
+require('./app/routes')(app, {});
+
+if (app.listen(port)) {
+	console.log("Live on port: " + port);
+};
+
